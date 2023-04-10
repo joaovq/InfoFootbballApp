ea9c22c25539d56143df8387a58421ab7e8fdf69
joaovq <vitorsnta90@gmail.com>
2023-04-10 17:00:07 -0300

    bug-fix: add kotlin-kapt for hilt instead ksp

--- app/src/main/java/br/com/joaovitorqueiroz/footballapi/InfoFootballApplication.kt
+++ app/src/main/java/br/com/joaovitorqueiroz/footballapi/InfoFootballApplication.kt
@@ -3,7 +3,9 @@ package br.com.joaovitorqueiroz.footballapi
 import android.app.Application
 import br.com.joaovitorqueiroz.footballapi.domain.di.containers.AppContainer
 import br.com.joaovitorqueiroz.footballapi.preferences.UserPreferences
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class InfoFootballApplication : Application() {
     val appContainer: AppContainer by lazy { AppContainer() }
     val prefs by lazy { UserPreferences(applicationContext) }
