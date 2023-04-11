f6a2c95350fa9b796085f41b276a09291575af3d
joaovq <vitorsnta90@gmail.com>
2023-04-10 23:15:48 -0300

    refactor: implements databinding

---
+++ app/src/main/java/br/com/joaovitorqueiroz/footballapi/ui/matches/adapter/MatchItemAdapter.kt
@@ -0,0 +1,44 @@
+package br.com.joaovitorqueiroz.footballapi.ui.matches.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import br.com.joaovitorqueiroz.footballapi.databinding.ItemsMatchBinding
+import br.com.joaovitorqueiroz.footballapi.ui.matchscreen.model.Match
+
+class MatchItemAdapter(val context: Context) :
+    ListAdapter<Match, MatchItemAdapter.MatchViewHolder>(DiffItemCallback) {
+    inner class MatchViewHolder(
+        private val binding: ItemsMatchBinding,
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(match: Match) {
+            binding.tvScore.text = " X "
+            binding.match = match
+            binding.executePendingBindings()
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MatchViewHolder {
+        val binding =
+            ItemsMatchBinding
+                .inflate(LayoutInflater.from(parent.context), parent, false)
+        return MatchViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MatchViewHolder, position: Int) {
+        val item = getItem(position)
+        holder.bind(item)
+    }
+
+    object DiffItemCallback : DiffUtil.ItemCallback<Match>() {
+        override fun areItemsTheSame(oldItem: Match, newItem: Match): Boolean =
+            oldItem == newItem
+
+        override fun areContentsTheSame(oldItem: Match, newItem: Match): Boolean =
+            oldItem.id == newItem.id
+    }
+}
