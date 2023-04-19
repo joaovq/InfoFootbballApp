b83e8750658c4cd26466a5380349ba09ac0a739a
joaovq <vitorsnta90@gmail.com>
2023-04-19 16:40:11 -0300

    refactor: replace glide to coil for load svg in extension

--- app/src/main/java/br/com/joaovitorqueiroz/footballapi/ui/util/extension/ImageViewExtension.kt
+++ app/src/main/java/br/com/joaovitorqueiroz/footballapi/ui/util/extension/ImageViewExtension.kt
@@ -5,13 +5,37 @@ import android.widget.ImageView
 import androidx.annotation.DrawableRes
 import androidx.core.net.toUri
 import br.com.joaovitorqueiroz.footballapi.R
+import coil.ImageLoader
+import coil.decode.SvgDecoder
+import coil.load
+import coil.request.ImageRequest
 import com.bumptech.glide.Glide
 
 fun ImageView.loadImage(
     context: Context = this.context,
     url: String,
     @DrawableRes error: Int = R.drawable.ic_error_default_image,
-) = Glide.with(context)
-    .load(url.toUri())
-    .error(error)
-    .into(this)
+) {
+    val imageLoader = ImageLoader.Builder(this.context)
+        .components {
+            SvgDecoder.Factory()
+        }
+        .build()
+
+    if (url.contains("svg")) {
+        this.load(url) {
+            decoderFactory { result, options, _ -> SvgDecoder(result.source, options) }
+        }
+    } else {
+        val request = ImageRequest.Builder(this.context)
+            .crossfade(true)
+            .crossfade(500)
+            /*.placeholder(R.drawable.placeholder)*/
+            .error(R.drawable.ic_error_default_image)
+            .data(url)
+            .target(this)
+            .build()
+
+        imageLoader.enqueue(request)
+    }
+}
