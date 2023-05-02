0d3a067151fdefaa4054e54b5823b0fe6c1a1d10
joaovq <vitorsnta90@gmail.com>
2023-05-02 20:55:28 -0300

    feature: map responses area and competition

--- app/src/main/java/br/com/joaovitorqueiroz/footballapi/ui/matchscreen/data/api/CompetitionResponse.kt
+++ app/src/main/java/br/com/joaovitorqueiroz/footballapi/ui/matchscreen/data/api/CompetitionResponse.kt
@@ -3,8 +3,8 @@ package br.com.joaovitorqueiroz.footballapi.ui.matchscreen.data.api
 data class CompetitionResponse(
     val id: Long,
     val area: AreaResponse?,
-    val name: String,
-    val code: String,
-    val type: String,
-    val emblem: String,
+    val name: String?,
+    val code: String?,
+    val type: String?,
+    val emblem: String?,
 )
